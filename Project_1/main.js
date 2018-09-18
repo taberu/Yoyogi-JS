0052a520c6a96c3f4876f1960ec67c0e3f967ce7
B <you@example.com>
2018-09-18 10:31:29 +0900

    some more changes

--- Project_1/main.js
+++ Project_1/main.js
@@ -1,36 +1,44 @@
-const Student = require("./Student.js");
+// import the Student class
+const StudentClassFromImport = require("./Student.js");
 
+// list of students 
 var students = new Array();
 
-// test
-var jack = new Student("Jack", "Smith", 1);
-var jill = new Student("Jill", "Smith", 2);
-console.log(jack.firstName);
-
-
-// stuff for project 
+// functions to manipulate student list
 function addStudent(list, student) {
+    // ToDo: should add check to make sure student is not in list already
     list.push(student);
 }
 
-function printStudents1(list){
+function removeStudent(list, student) {
+
+}
+
+function printStudents(list){
     list.forEach(function(element){
         console.log(element.firstName);
     });
+
+    // this does the same thing, but uses => fat arrow function
+
+    // list.forEach(element => {
+    //     console.log(element.firstName);
+    // });
 }
 
-function printStudents2(list) {
-    list.forEach(element => {
-        console.log(element.firstName);
-    });
-}
+// testing
+
+var jack = new StudentClassFromImport("Jack", "Smith", 1);
+var jill = new StudentClassFromImport("Jill", "Smith", 2);
+console.log(jack.firstName);
 
 addStudent(students, jack);
 addStudent(students, jill);
-printStudents1(students);
-printStudents2(students);
+
+printStudents(students);
 
 jack.addTestScore(10);
 jack.addTestScore(30);
+
 jack.printTestScore();
 
