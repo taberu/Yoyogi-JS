13d4f0cb43f6095879d9f1aafd1c62be70ef1f16
B <you@example.com>
2019-02-08 09:33:50 +0900

    updates

--- nodejs-readline/user_input._recursive.js
+++ nodejs-readline/user_input._recursive.js
@@ -5,8 +5,6 @@ const rl = readline.createInterface({
     output : process.stdout
 })
 
-console.log("1");
-
 function askQuestionsRecursively(){
     rl.question('What is your favorite color? ', (answer) => {
         if (answer == 'exit') {
